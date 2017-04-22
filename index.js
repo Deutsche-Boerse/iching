60f3ed008eac4fffa6f7d0e3b7991f8d2ec8d970
Dave de Sade <davesade.42@gmail.com>
2017-04-22 21:03:56 +0200

    First testing handler.

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 'use strict';
+var Alexa = require('alexa-sdk');
+var appId = process.env.APPID; // Set as environmental variable in Lambda itself.
 
+/*
 exports.handler = (event, context, callback) => {
     //console.log('Received event:', JSON.stringify(event, null, 2));
     console.log('value1 =', event.key1);
@@ -8,4 +11,20 @@ exports.handler = (event, context, callback) => {
     callback(null, event.key1);  // Echo back the first key value
     //callback('Something went wrong');
 };
+*/
 
+exports.handler = function(event, context, callback) {
+  var alexa = Alexa.handler(event, context);
+  alexa.registerHandlers(handlers);
+  alexa.appId = appId;
+  alexa.dynamoDBTableName = 'ichingusers';
+  alexa.execute();
+};
+
+var handlers = {
+
+    'ichingIntent': function () {
+        this.emit(':tell', 'Iching listens to you!');
+    }
+
+};
