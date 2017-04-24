e7967fb53fe4389c6bb6841596ab80143a10d343
kubedav <davesade.42@gmail.com>
2017-04-24 10:00:27 +0200

    Removed unwanted functions (buggy).

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 var Alexa = require('alexa-sdk');
 var appId = process.env.APPID; // Set as environmental variable in Lambda itself.
-var apiCall = require('./bf_api.js');
+//var apiCall = require('./bf_api.js');
 
 exports.handler = function(event, context, callback) {
     var alexa = Alexa.handler(event, context);
@@ -25,7 +25,7 @@ var handlers = {
     'iching': function() {
         this.attributes['name'] = 'Dave'; // This saves to DynamoDB a value 'Dave' to column 'name'.
         var isin = 'DE0008404005';
-        apiCall.getPrice(isin); //
+        //apiCall.getPrice(isin); //
         this.emit(':tell', 'Iching listens to you! ' + isin);
     },
     'wait': function() {
